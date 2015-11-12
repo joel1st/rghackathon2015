55b73020b90ce55dd3ed50a134e3a809f545b423
Tino Fuhrmann <tino-fuhrmann@web.de>
2015-11-12 06:33:29 +0100

    Modified dumpRequest to work with the post request from the api

--- dumpRequest.php
+++ dumpRequest.php
@@ -1,10 +1,16 @@
 <?php
+$inputJSON = file_get_contents('php://input');
+echo $inputJSON;
+echo "POST<br>";
+var_dump($_POST);
+echo "<br>GET<br>";
+var_dump($_GET);
 
 ob_start();
 var_dump($_POST);
 var_dump($_GET);
 $result = ob_get_clean();
-
+$result .= "input" .$inputJSON;
 $rand = substr(md5(microtime()),rand(0,26),5);
 
 file_put_contents($rand ."txt", $result);
