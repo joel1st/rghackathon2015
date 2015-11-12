92b54ce833c5eb128f36cf63467a8071093552cf
tcaldwel@nmsu.edu <tcaldwel@nmsu.edu>
2015-11-11 16:46:39 -0800

    Add server and API boilerplate code

---
+++ api/api.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var router = express.Router();
+var uuid = require('node-uuid');
+var os = require('os');
+
+// api -------------------------
+/* 
+Send whatever data is needed 
+for signup to front end 
+(split into multiple endpoints if needed.)
+*/
+router.route('/sign_up')
+	.get(function(req, res) {
+		res.send('signup');
+	});
+
+router.route('/filters')
+	.get(function(req, res) {
+		res.send('filters');
+	});
+
+router.route('/generate')
+	.get(function(req, res) {
+		res.send('generate');
+	});
+
+// Generate a callback
+router.route('/generate_callback')
+	.get(function(req, res) {
+		res.send(uuid.v1());
+	});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var api = require('./api/api.js');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+
+// API to communicate with frontend
+app.use('/api', api);
+
+app.get('/', function(req, res) {
+	res.sendFile('public/index.html', {'root' : __dirname});
+});
+
+app.listen(1337, function() {
+	console.log('Server running at http://127.0.0.1:1337/');
+});
