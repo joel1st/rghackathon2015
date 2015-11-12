e7cb386eb416d55d09fc33ea48c2fe34a9962510
joel1st <joel_1st@hotmail.com>
2015-11-12 13:44:15 +1000

    removed comments from old project

--- models/tournaments.js
+++ models/tournaments.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 var config = require('../config.js');
-/**
- * Used to build up the long champ list on the homepage and the
- * individual champion roles on the left hand side of the champion pages.
- */
+
 var tournamentSchema = new mongoose.Schema({
     ownerId: String,
     tournamentId: String,
