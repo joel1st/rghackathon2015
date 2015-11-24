e78744424d5af795f26a70049d76cbc5e8c64e18
Taylor Caldwell <tcaldwel@nmsu.edu>
2015-11-24 12:01:33 -0700

    iframe pls

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	db: {
 		name: process.env.DB_NAME || 'rghackathon2015'
 	},
-	callbackUrl: "https://tournamentgg.herokuapp.com",
+	callbackUrl: "http://tournamentgg.herokuapp.com",
 	supportedRegions: ['NA'],
 	pickTypes: ['DRAFT_MODE', 'BLIND_PICK', 'ALL_RANDOM', 'TOURNAMENT_DRAFT'],
 	spectateTypes: ['ALL', 'NONE', 'LOBBYONLY'],
